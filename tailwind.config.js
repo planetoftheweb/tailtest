82461deeec2db48d3f7216785e5f0bbd5784c053
Ray Villalobos <ray@planetoftheweb.com>
2021-08-31 23:06:34 +0000

    working on temate

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['index.html'],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
